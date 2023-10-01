d1f047be68a182aec7c1cca7b38c252573fa4443
SpaceDust75 <aacc9058@gmail.com>
2023-10-02 00:17:26 +0900

    feat: "버튼 ui 변경"
    - 이미지 파일 선택 버튼과 웹캠 사진 촬영 버튼 분리

--- static/js/script.js
+++ static/js/script.js
@@ -1,60 +1,57 @@
 function previewImg1(input) {
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
-    reader.onload = function(e) {
-      document.getElementById('preview1').src = e.target.result;
-    };
-    reader.readAsDataURL(input.files[0]);
-  } else {
-    document.getElementById('preview1').src = "";
-  }
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            document.getElementById('preview1').src = e.target.result;
+        };
+        reader.readAsDataURL(input.files[0]);
+    } else {
+        document.getElementById('preview1').src = "";
+    }
 }
 
 function previewImg2(input) {
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
-    reader.onload = function(e) {
-      document.getElementById('preview2').src = e.target.result;
-    };
-    reader.readAsDataURL(input.files[0]);
-  } else {
-    document.getElementById('preview2').src = "";
-  }
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            document.getElementById('preview2').src = e.target.result;
+        };
+        reader.readAsDataURL(input.files[0]);
+    } else {
+        document.getElementById('preview2').src = "";
+    }
 }
 
-function call_confirm1(){
-	if(confirm("이미지 업로드 방식 선택")){
-		alert("웹캡 실행");
-	}else{
-        var input = document.createElement("input");
-        input.type = "file";
-        input.accept = "image/jpeg"; // 이미지 확장자를 jpeg로 설정
-
-        input.style.display = "none";
+function takeWebcam1() {
+    if (confirm("촬영하시려면 확인 버튼 클릭")) {
+        captureImage1();
+    } else {
+        var input = document.getElementsByName("image1");
+        // var input = document.createElement("input");
+        // input.type = "file";
+        // input.accept = "image/jpeg"; // 이미지 확장자를 jpeg로 설정
+        //
+        // input.style.display = "none";
         input.addEventListener("change", function () {
             var selectedFile = input.files[0];
             if (selectedFile) {
                 previewImg1(input)
             }
         });
-
-        // input 엘리먼트를 현재 문서에 추가
-        document.body.appendChild(input);
-
-        // input 엘리먼트를 클릭하여 이미지 선택 창을 열도록 함
         input.click();
-	}
+    }
 }
 
-function call_confirm2(){
-	if(confirm("이미지 업로드 방식 선택")){
-		alert("웹캡 실행");
-	}else{
-        var input = document.createElement("input");
-        input.type = "file";
-        input.accept = "image/jpeg"; // 이미지 확장자를 jpeg로 설정
-
-        input.style.display = "none";
+function takeWebcam2() {
+    if (confirm("촬영하시려면 확인 버튼 클릭")) {
+        captureImage2();
+    } else {
+        var input = document.getElementsByName("image2");
+        // var input = document.createElement("input");
+        // input.type = "file";
+        // input.accept = "image/jpeg"; // 이미지 확장자를 jpeg로 설정
+        //
+        // input.style.display = "none";
         input.addEventListener("change", function () {
             var selectedFile = input.files[0];
             if (selectedFile) {
@@ -62,5 +59,83 @@ function call_confirm2(){
             }
         });
         input.click();
-	}
+    }
 }
+
+// 웹캠으로 사진 촬영
+function captureImage1() {
+    navigator.mediaDevices.getUserMedia({video: true})
+        .then(function (stream) {
+            const video = document.createElement('video');
+            document.body.appendChild(video);
+            video.srcObject = stream;
+            video.play();
+
+            video.addEventListener('canplay', function () {
+                const canvas = document.createElement('canvas');
+                canvas.width = video.videoWidth;
+                canvas.height = video.videoHeight;
+                canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+
+                const img = document.getElementById('preview1');
+                img.src = canvas.toDataURL('image/jpeg', 0.8); // 이미지 포맷 및 품질 조절
+
+                // 웹캠 스트림 종료
+                stream.getTracks().forEach(track => track.stop());
+
+                // 촬영 완료 메시지 표시
+                alert('촬영 완료 XD');
+            });
+        })
+        .catch(function (error) {
+            console.error('웹캠에 액세스할 수 없습니다:', error);
+        });
+}
+
+function captureImage2() {
+    navigator.mediaDevices.getUserMedia({video: true})
+        .then(function (stream) {
+            const video = document.createElement('video');
+            document.body.appendChild(video);
+            video.srcObject = stream;
+            video.play();
+
+            video.addEventListener('canplay', function () {
+                const canvas = document.createElement('canvas');
+                canvas.width = video.videoWidth;
+                canvas.height = video.videoHeight;
+                canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+
+                const img = document.getElementById('preview2');
+                img.src = canvas.toDataURL('image/jpeg', 0.8); // 이미지 포맷 및 품질 조절
+
+                // 웹캠 스트림 종료
+                stream.getTracks().forEach(track => track.stop());
+
+                // 촬영 완료 메시지 표시
+                alert('촬영 완료 XD');
+            });
+        })
+        .catch(function (error) {
+            console.error('웹캠에 액세스할 수 없습니다:', error);
+        });
+}
+
+// 이미지 업로드 처리
+document.getElementById('preview2').addEventListener('click', function () {
+    document.getElementById('fileInput').click();
+});
+
+document.getElementById('fileInput').addEventListener('change', function (event) {
+    const file = event.target.files[0];
+    if (file) {
+        const img = document.getElementById('preview2');
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+            img.src = e.target.result;
+        };
+
+        reader.readAsDataURL(file);
+    }
+});
