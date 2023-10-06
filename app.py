8bf2132b714e500d091263da01e9273b55d32ac9
kongji <kongji4092@gmail.com>
2023-10-06 22:12:39 +0900

    style: "이전 blend 소스 제거"

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-from io import BytesIO
-
 import qrcode
 import logging
 from flask import Flask, render_template, request, redirect
@@ -8,6 +6,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 CORS(app)
+
 
 def generate_qrcode(data):
     """
@@ -27,43 +26,6 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/blend', methods=['POST'])
-def blend():
-    app.logger.info('This is an info message')
-    """
-    이미지 두 장을 받아서 AI 모델을 이용해서 합친후 출력
-    :return:
-    """
-
-    image1 = request.files['preview1']
-    image2 = request.files['preview2']
-
-    # BytesIO 객체 생성
-    image1_stream = BytesIO(image1.read())
-    image2_stream = BytesIO(image2.read())
-
-    # 이미지를 열어서 처리
-    # image1_pil = Image.open(image1_stream)
-    # image2_pil = Image.open(image2_stream)
-
-    # 이미지 처리 로직
-
-    # 처리된 이미지를 BytesIO 형태로 변환하여 전달
-    # result_image_stream = BytesIO()
-    # result_image_stream.
-
-    import base64
-    result_image1_base64 = base64.b64encode(image1_stream.getvalue()).decode('utf-8')
-    result_image1_base64 = "data:image/png;base64,{}".format(result_image1_base64)
-    result_image2_base64 = base64.b64encode(image2_stream.getvalue()).decode('utf-8')
-    result_image2_base64 = "data:image/png;base64,{}".format(result_image2_base64)
-
-    app.logger.info(image1.content_type)
-    app.logger.info(image2.content_type)
-
-
-    return render_template("result.html", result_image1 = result_image1_base64, result_image2=result_image2_base64)
-
 @app.route('/test', methods=['POST'])
 def test():
     app.logger.info('This is an info message')
@@ -72,7 +34,12 @@ def test():
 
     app.logger.info("테스트 중 입니다")
 
+    # 이미지 2개 받아서 합성
+    # ouput img
+    # result html에 여러장의 사진을 보내야함(10장)
+
     return render_template('result.html', result_image1=img1, result_image2=img2)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=80)
