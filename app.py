b20c13428ffd370d8cba2edf9cd7848ed95215ae
SpaceDust75 <aacc9058@gmail.com>
2023-10-29 18:09:29 +0900

    feat: "이미지 저장 및 디자인"
    - 버튼 클릭 시 이미지 2개 저장
    - 배경 화면 matrix rain 애니메이션 추가
    - webcam frame 이미지 추가

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from io import BytesIO
-from PIL import Image
+
 import qrcode
 import logging
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -16,7 +16,7 @@ def generate_qrcode(data):
     :return: QR코드 이미지
     """
     qr = qrcode.QRCode(version=1, box_size=10, border=5)
-    qr.addata(data)
+    qr.add_data(data)
     qr.make(fit=True)
     img = qr.make_image(fill='black', back_color='white')
     return img
@@ -27,46 +27,15 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/blend', methods=['POST'])
-def blend():
+@app.route('/test', methods=['POST'])
+def test():
     app.logger.info('This is an info message')
-    """
-    이미지 두 장을 받아서 AI 모델을 이용해서 합친후 출력
-    :return:
-    """
+    img1 = request.form.get('preview1')
+    img2 = request.form.get('preview2')
 
-    image1 = request.files['image1']
-    image2 = request.files['image2']
+    app.logger.info("테스트 중 입니다")
 
-    # BytesIO 객체 생성
-    image1_stream = BytesIO(image1.read())
-    image2_stream = BytesIO(image2.read())
-
-    # 이미지를 열어서 처리
-    image1_pil = Image.open(image1_stream)
-    image2_pil = Image.open(image2_stream)
-
-    # 이미지 처리 로직
-
-    # 처리된 이미지를 BytesIO 형태로 변환하여 전달
-    # result_image_stream = BytesIO()
-    # result_image_stream.
-
-    import base64
-    # result_image_base64 = base64.b64encode(result_image_stream.getvalue()).decode('utf-8')
-    result_image1_base64 = base64.b64encode(image1_stream.getvalue()).decode('utf-8')
-    result_image1_base64 = "data:image/png;base64,{}".format(result_image1_base64)
-    result_image2_base64 = base64.b64encode(image2_stream.getvalue()).decode('utf-8')
-    result_image2_base64 = "data:image/png;base64,{}".format(result_image2_base64)
-
-    app.logger.info(image1.content_type)
-    app.logger.info(image2.content_type)
-
-    # ai 로직 구현
-
-    # return render_template('blend.html', image=image)
-    return render_template("result.html", result_image1 = result_image1_base64, result_image2=result_image2_base64)
-
+    # return render_template('result.html', result_image1=img1, result_image2=img2)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=80)
--- static/js/script.js
+++ static/js/script.js
@@ -42,6 +42,7 @@ function takeWebcam1() {
     setTimeout(function () {
         if (confirm("촬영하시려면 확인 버튼 클릭")) {
             captureImage1();
+            capturedImage1 = true;
         } else {
             var input = document.getElementsByName("image1")[0];
             input.addEventListener("change", function () {
@@ -73,6 +74,7 @@ function takeWebcam2() {
     setTimeout(function () {
         if (confirm("촬영하시려면 확인 버튼 클릭")) {
             captureImage2();
+            capturedImage2 = true;
         } else {
             var input = document.getElementsByName("image2")[0];
             input.addEventListener("change", function () {
@@ -106,6 +108,7 @@ function captureImage1() {
 
                 const img = document.getElementById('preview1');
                 img.src = canvas.toDataURL('image/png', 0.8); // 이미지 포맷 및 품질 조절
+
 
                 // 웹캠 스트림 종료
                 stream.getTracks().forEach(track => track.stop());
@@ -152,24 +155,111 @@ function captureImage2() {
         });
 }
 
-// 웹캠 영상 활성화 모습
+// 배경 하트 matrix rain 애니메이션 구현
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Symbol {
+    constructor(x, y, fontSize, canvasHeight) {
+        this.characters =
+            "♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥";
+        this.x = x;
+        this.y = y;
+        this.fontSize = fontSize;
+        this.text = "";
+        this.canvasHeight = canvasHeight;
+    }
+
+    draw(context) {
+        this.text = this.characters.charAt(
+            Math.floor(Math.random() * this.characters.length)
+        );
+        context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
+        if (this.y * this.fontSize > this.canvasHeight && Math.random() > 0.98) {
+            this.y = 0;
+        } else {
+            this.y += 1;
+        }
+    }
+}
+
+class Effect {
+    constructor(canvasWidth, canvasHeight) {
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+        this.fontSize = 25;
+        this.columns = this.canvasWidth / this.fontSize;
+        this.symbols = [];
+        this.#initialize();
+        console.log(this.symbols);
+    }
+
+    #initialize() {
+        for (let i = 0; i < this.columns; i++) {
+            this.symbols[i] = new Symbol(i, 0, this.fontSize, this.canvasHeight);
+        }
+    }
+
+    resize(width, height) {
+        this.canvasWidth = width;
+        this.canvasHeight = height;
+        this.columns = this.canvasWidth / this.fontSize;
+        this.symbols = [];
+        this.#initialize();
+    }
+}
+
+const effect = new Effect(canvas.width, canvas.height);
+let lastTime = 0;
+const fps = 15;
+const nextFrame = 1000 / fps;
+let timer = 0;
+
+function animate(timeStamp) {
+    const deltaTime = timeStamp - lastTime;
+    lastTime = timeStamp;
+    if (timer > nextFrame) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = "deeppink";
+        ctx.font = effect.fontSize + "px monospace";
+        effect.symbols.forEach((symbol) => symbol.draw(ctx));
+        timer = 0;
+    } else {
+        timer += deltaTime;
+    }
+    requestAnimationFrame(animate);
+}
+
+window.addEventListener("resize", function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    effect.resize(canvas.width, canvas.height);
+});
+
+requestAnimationFrame(animate);
 
 
-// 이미지 업로드 처리
-// document.getElementById('preview2').addEventListener('click', function () {
-//     document.getElementById('fileInput').click();
-// });
+// webcam으로 찍은 이미지 저장
+function downloadImages() {
+    // 두 개의 이미지 데이터 URL 및 파일 이름 가져오기
+    const dataURL1 = document.getElementById('preview1').src;
+    const filename1 = 'webcam_image_1.png';
 
-// document.getElementById('fileInput').addEventListener('change', function (event) {
-//     const file = event.target.files[0];
-//     if (file) {
-//         const img = document.getElementById('preview2');
-//         const reader = new FileReader();
-//
-//         reader.onload = function (e) {
-//             img.src = e.target.result;
-//         };
-//
-//         reader.readAsDataURL(file);
-//     }
-// });
+    const dataURL2 = document.getElementById('preview2').src;
+    const filename2 = 'webcam_image_2.png';
+
+    // 이미지 다운로드 함수 호출
+    downloadImage(dataURL1, filename1);
+    downloadImage(dataURL2, filename2);
+}
+
+function downloadImage(dataURL, filename) {
+            const a = document.createElement('a');
+            a.href = dataURL;
+            a.download = filename;
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        }
